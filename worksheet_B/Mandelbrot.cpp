0bf3bea53f97bb5b642bf024576687d72883f2b4
Kieran Goldsworthy <kieranalexstrasza@gmail.com>
2017-02-05 23:42:24 +0000

    Update Mandelbrot.cpp

--- worksheet_B/Mandelbrot.cpp
+++ worksheet_B/Mandelbrot.cpp
@@ -18,18 +18,68 @@ int main()
 	for (int pixelY = 0; pixelY < image.height(); pixelY++)
 	{
 		// TODO: Map the y coordinate into the range minY to maxY
-		//double y0 =
+		double y0 = (pixelY / 800.0) * (maxY - minY) + minY;
 
 		for (int pixelX = 0; pixelX < image.width(); pixelX++)
 		{
 			// TODO: Map the x coordinate into the range minX to maxX
-			//double x0 =
+			double x0 = ((pixelX / 800.0) * (maxX - minX) + minX);
 
 			// TODO: implement the algorithm to colour a single pixel (x0, y0) of the Mandelbrot set fractal
 			// The code below simply fills the screen with random pixels
+			double xI = 0;
+			double yI = 0;
+			int iteration = 0;
+			int max_iteration = 200;
 
+			while (iteration < maxIteration && (x*x) + (y*y) < 4)
+			{
+				double xTemp = (x*x) - (y*y) + x0;
+				y = 2 * x * y + y0;
+				x = xTemp;
+				iteration++;
+			}
 			// Write the pixel
-			// TODO: change the right-hand side of these three lines to write the desired pixel colour value
+
+		}
+
+		int red = 0;
+		int green = 0;
+		int blue = 0;
+		int maxValue = 255;
+
+		if (iteration <= 60)
+		{
+			green = maxValue;
+			red = (iteration / 60) * maxValue;
+		}
+
+		else if (iteration <= 120)
+		{
+			red = maxValue;
+			green = maxValue - (((iteration - 60) / 60) * maxValue);
+		}
+
+		else if (iteration <= 180)
+		{
+			blue = maxValue;
+			green = maxValue - (((iteration - 120) / 60) * maxValue);
+		}
+
+		else if (iteration <= 199)
+		{
+			red = maxValue;
+			blue = maxValue - (((iteration - 60) / 60) * maxValue);
+		}
+
+		else
+		{
+			red = 0;
+			green = 0;
+			blue = 0;
+		}
+
+
 			image(pixelX, pixelY, 0, 0) = rand(); // red component
 			image(pixelX, pixelY, 0, 1) = rand(); // green component
 			image(pixelX, pixelY, 0, 2) = rand(); // blue component
@@ -37,14 +87,14 @@ int main()
 
 		// Uncomment this line to redisplay the image after each row is generated
 		// Useful if your program is slow and you want to verify that it is actually doing something
-		//display.display(image);
+		display.display(image);
 	}
 
 	// Display the complete image
 	display.display(image);
 
 	// Uncomment this line to save the image to disk
-	//image.save_bmp("mandelbrot.bmp");
+	image.save_bmp("mandelbrot.bmp");
 
 	// Wait for the window to be closed
 	while (!display.is_closed())
